40b9f4657ee632f1eda2f1bfd8db87e42d9f1335
GianEd36 <gcenteno_19ur0202@psu.edu.ph>
2026-02-03 21:18:03 +0800

    init

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Assignment 2</title>
+</head>
+<body>
+    @php
+        $name = "Centeno, Gian Edmar S."
+    @endphp
+    <h1 style="color:blue;">Name: {{ $name }}</h1>
+</body>
+</html>
